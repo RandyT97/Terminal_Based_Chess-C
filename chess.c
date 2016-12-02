22381336dbb82628bb09da79ec75465ef57efb84
Randy <rtsui9797@gmail.com>
2016-12-01 23:48:06 -0500

    Added ability to assign players to sides maybe

--- chess.c
+++ chess.c
@@ -42,13 +42,15 @@ int isCheckMate(char board[8][8]);
 void notate(int x, int y);
 void playerMenu(FILE * ifp);
 void updateProfiles(FILE * ifp,struct player base[20]);
-void playStandard();
+void playStandard(struct player upperCase, struct player lowerCase);
 //void printProfiles(FILE * ifp);
 //void scanProfiles(FILE * ifp);
 void createProfile(struct player base[20],int playercounter);
 void loadProfiles(FILE * ifp,struct player base[20]);
 void updateProfiles(FILE * ifp,struct player base[20]);
 void printProfile(struct player base[20]);
+int isEnd(char board[8][8]);
+int findProfile(struct player base[20],char fname[20]);
 int main() {
   int input=5;
   char fileexists;
@@ -76,8 +78,8 @@ int main() {
 void playerMenu(FILE * ifp) {
   int input=0;
   int playercounter=0;
-  char username[LENGTH];
-  char username2[LENGTH];
+  char capfname[20];
+  char lowfname[20];
   struct player database[20];
   loadProfiles(ifp,database);
   for(int i=0;i<20;i++) {
@@ -96,7 +98,11 @@ void playerMenu(FILE * ifp) {
     scanf("%d",&input);
     switch(input) {
       case(1):
-        playStandard();
+        printf("First name of the player in control of upper case pieces?\n");
+        scanf("%s",capfname);
+        printf("First name of the player in control of upper case pieces?\n");
+        scanf("%s",lowfname);
+        playStandard(database[findProfile(database,capfname)], database[findProfile(database,lowfname)]);
         break;
       case(2): {
         //createProfile(database,playercounter);
@@ -147,9 +153,37 @@ void printProfile(struct player base[20]) {
     printf("Firstname:%s\tLastname:%s\tWins:%d\tLosses:%d\t\n",base[i].fname,base[i].lname,base[i].win,base[i].loss);
   }
 }
-
+int findProfile(struct player base[20],char fname[20]) {
+  int index;
+  for(int i=0;i<20;i++) {
+    if(strcmp(base[i].fname,fname)==0)
+      index = i;
+  }
+  return index;
+}
+int isEnd(char board[8][8]) {
+  int lowerKingAlive=0;
+  int upperKingAlive=0;
+  for(int i=0;i<8;i++) {
+    for(int j=8;j<8;j++) {
+      if(board[i][j]=='K')
+        lowerKingAlive=1;
+      if(board[i][j]=='k')
+        upperKingAlive=1;
+    }
+  }
+  if(!lowerKingAlive) {
+    printf("Game is over, upper case won!\n");
+    return 1;
+  }
+  if(!upperKingAlive) {
+    printf("Game is over, lower case won!\n");
+    return 2;
+  }
+  return 0;
+}
 // STANDARD BOARD MATERIALS
-void playStandard() {//DEPENDS ON PRINTOUT LOCATIONS!!!
+void playStandard(struct player upperCase, struct player lowerCase) {//DEPENDS ON PRINTOUT LOCATIONS!!!
   int game = 1;
   int initialx;
   int initialy;
@@ -166,6 +200,16 @@ void playStandard() {//DEPENDS ON PRINTOUT LOCATIONS!!!
     scanf("%d",&finalx);
     scanf("%d",&finaly);
     move(standardboard, initialx, initialy, finalx, finaly);
+    if(isEnd(standardboard)!=0) {
+      if(isEnd(standardboard)==1) {
+        upperCase.win+=1;
+        game=0;
+      }
+      else{
+        lowerCase.win+=1;
+        game=0;
+      }
+    }
   }
 }
 //Populates board
